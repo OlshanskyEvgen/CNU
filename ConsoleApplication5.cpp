18fee347aaa42b0b9bb9097fae1af1022dcef7b8
Olshanskiy_Evgen <citripio20@gmail.com>
2024-10-20 15:34:08 +0300

    add nef func

--- ConsoleApplication5.cpp
+++ ConsoleApplication5.cpp
@@ -5,21 +5,17 @@
 #include <windows.h>
 #include <math.h>
 using namespace std;
+//qdqwdqwdqwdwdqwdqwdqwd
 
 int main()
 {
-    double x11, x12, x13, x21, x22, x23, x31, x32, x33, F ;
-    cout << "enter " << "x11 " << "x12 " << "x13 " << endl;
-    cin >> x11 >> x12 >> x13 ;
-    cout << "enter " << "x21 " << "x22 " << "x23 " << endl;
-    cin >> x21 >> x22 >> x23;
-    cout << "enter " << "x31 " << "x32 " << "x33 " << endl;
-    cin >> x31 >> x32 >> x33;
-    F = (x11*x22*x33) + (x12*x23*x31) + (x13* x21* x32) - (x13* x22 * x31) - (x11 * x23 * x32) - (x12 * x21 * x33);
-    cout << F;
-
-    
-   
+    double x11, x12, x21, x22, F ;
+    cout << "enter " << "x11"  << endl;
+    cin >> x11 >> x12;
+    cout << "enter " << "x21"  << endl;
+    cin >> x21 >> x22;
+    F = x11 * x22 - x12 * x21 ;
+    cout << F;  
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
